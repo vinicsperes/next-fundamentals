04edf6622a2a279d38b0238167e6b86e54e553d3
vinicsperes@gmail.com <peresvini2@gmail.com>
2023-03-08 23:44:53 -0300

    Create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
-import { styled } from "../styles"
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import camiseta1 from '../assets/camisetas/Variant6.png'
+import camiseta2 from '../assets/camisetas/Variant7.png'
+import camiseta3 from '../assets/camisetas/Variant8.png'
 
 export default function Home() {
   return (
-    <>
-      <Button>Enviar</Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
