081a6e49a0d782bd7c3a8c7ccf41a1d3276a34fb
vinicsperes@gmail.com <peresvini2@gmail.com>
2023-03-11 22:23:20 -0300

    Structuring product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { ProductContainer, ImageContainer, ProductDetails } from '../product';
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <h1>Sucess!: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>RS 79,90</span>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius commodi deleniti fugit sequi modi molestiae placeat rerum similique nam qui? Sunt, similique eveniet repellat et distinctio delectus cum? Voluptatum, cumque!</p>
+
+        <button>
+          Comprar Agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
